605b6d5b027d51b7c244662282642b1d0be9b034
Gordon Gibb <g.gibb@epcc.ed.ac.uk>
2018-08-25 00:34:01 +0100

    Logger now produces daily graphs, and a graph of the last 24h of data

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,8 @@ import os
 import sys
 import datetime
 import time
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
 
 logdir = "/home/pi/logs"
 
@@ -39,8 +41,12 @@ class Logger:
         
         self.logfile=None
         
+        self.cachedata()
+        
         #open logfile for writing
         self.openLog()
+        
+        self.lasttime=time.time()
     
     #opens a logfile for writing to
     def openLog(self):
@@ -65,6 +71,10 @@ class Logger:
         #check that it's still 'today'. If not, make new logfile
         date = str(datetime.date.today())
         if date != self.today:
+            #make a daily summary plot
+            self.prune_old_values()
+            self.plot(title=self.today)
+            
             print("Logger: New day, new logfile")
             self.today=date
             self.logfile.close()
@@ -86,6 +96,152 @@ class Logger:
         
         self.logfile.write("\n")
         self.logfile.flush()
+        
+        #append data to historical cache
+        #first offset the historical 'time since latest measurement' by our new measurement time
+        dt = time.time() - self.lasttime
+        self.lastime = time.time()
+        for i in range(len(self.historical["seconds"])):
+            self.historical["seconds"][i] -= dt
+        #now add our new data
+        self.historical["seconds"].append(0)
+        for name in self.names:
+            self.historical[name].append(args.get(name))
+        #now prune old data
+        self.prune_old_values()
+        
+        #finally plot the new summary
+        self.plot("Latest")
+            
+        self.lasttime=time.time()
+        
+                
+        
+    
+    # store the last 24h of data for logging purposes    
+    # This is called once during init to read in any existing log files
+    # afterwards this cache is updated each time a measurement is logged
+    # in the self.log() method
+    def cachedata(self):
+        #get today's date and yesterday's date
+        today=datetime.date.today()
+        yesterday = today - datetime.timedelta(days=1)
+        
+        #get filenames for today and yesterday
+        yesterdayfile = self.dir+"/"+self.name+"_"+str(yesterday)+".log"
+        todayfile = self.dir+"/"+self.name+"_"+str(today)+".log"
+        
+        #create dictionary to store the data in
+        self.historical = {}
+        self.historical["seconds"] = []
+        self.historical["time"] = []
+        for name in self.names:
+            self.historical[name] = []
+        
+        day = 3600*24
+        
+        #see if yesterday's file exists - if so, read it
+        print(yesterdayfile)
+        if os.path.isfile(yesterdayfile):
+            f=open(yesterdayfile,"r")
+            for line in f:
+                if line[0] == "#": continue
+                data=line.split()
+                t = int(data[1])-day
+                self.historical["seconds"].append(t)
+                i=2
+                for name in self.names:
+                    try:
+                        val = float(data[i])
+                    except:
+                        val = 0
+                    self.historical[name].append(val)
+                    i+=1
+            f.close()
+        #see if today's file exists - if so, read it
+        print(todayfile)
+        if os.path.isfile(todayfile):
+            f=open(todayfile,"r")
+            for line in f:
+                if line[0] == "#": continue
+                data=line.split()
+                t = int(data[1])
+                self.historical["seconds"].append(t)
+                i=2
+                for name in self.names:
+                    try:
+                        val = float(data[i])
+                    except:
+                        val = 0
+                    self.historical[name].append(val)
+                    i+=1
+            f.close()
+        
+        #determine current time from midnight in seconds
+        seconds=self.get_seconds_from_midnight()
+        
+        #store the time we define as zero in our historical log (i.e. now)
+        self.lasttime=time.time()
+        
+        #subtract from the array of seconds to get time before now
+        
+        for i in range(len(self.historical["seconds"])):
+            self.historical["seconds"][i] -= seconds
+        
+        #remove values older than a day
+        self.prune_old_values()
+        
+        
+        self.plot("Latest")
+        
+        
+        
+    def prune_old_values(self):
+        #seconds in a day
+        day = 3600*24
+        
+        #prune off the values older than a day
+        while True:
+            if self.historical["seconds"][0] < -day:
+                self.historical["seconds"].pop(0)
+                for name in self.names:
+                    self.historical[name].pop(0)
+            else:
+                break
+        
+    def get_seconds_from_midnight(self):
+        t=datetime.datetime.now()
+        hh=t.hour
+        mm=t.minute
+        ss=t.second
+        seconds = hh*3600 + mm*60 +ss
+        return seconds
+    
+    #make plots of the last 24h of data    
+    def plot(self,title):
+        now=datetime.datetime.now()
+        t=[]
+        for tm in self.historical["seconds"]:
+            t.append(now + datetime.timedelta(seconds=tm))
+        
+        t=np.asarray(t)
+        
+        fmt=dates.DateFormatter("%H:%M\n %d/%m")
+
+        
+        for name in self.names:
+            v=[]
+            for x in self.historical[name]:
+                v.append(x)
+            v=np.asarray(v)
+            fig, ax = plt.subplots()
+            plt.plot(t,v)
+            plt.title(title)
+            plt.ylabel(name)
+            ax.xaxis.set_major_formatter(fmt)
+            plt.savefig(self.dir+"/"+name+"_"+title+".png")
+            plt.clf()
+        
     
     def finalise(self):
         print("Logger: Finalising")
@@ -99,13 +255,8 @@ class Logger:
                  
 if __name__ == "__main__":
         
-    log=Logger("testlog",2,["a","b"])
+    log=Logger("TempHum",3,["Temperature","Humidity", "attempts"])
     
-    for i in range(10):
-        a=np.random.random()
-        b=np.random.random()
-        log.log(a=a,b=b)
-        time.sleep(1)
         
         
         
